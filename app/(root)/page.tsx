4c72535938480de404dfd495d3482cf98b05f236
awais512 <raza.awais25@gmail.com>
2024-02-17 08:42:43 +0000

    Clerk Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/sign-in" />
+    </div>
+  );
 }
