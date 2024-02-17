0fb709e8561741cbf850647680d7ef9c279a22fd
awais512 <raza.awais25@gmail.com>
2024-02-17 23:30:31 +0000

    Header Component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/shared/header";
 import React from "react";
 
 const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+  return (
+    <Header title="Transformation Title" subtitle="Transformation subtitle" />
+  );
 };
 
 export default AddTransformationTypePage;
