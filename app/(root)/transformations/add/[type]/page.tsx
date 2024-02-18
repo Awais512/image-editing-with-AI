3f5f19288883986acc5c33dd2a8a774eae210f38
awais512 <raza.awais25@gmail.com>
2024-02-18 00:04:22 +0000

    Custom Input field component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,33 @@
-import Header from "@/components/shared/header";
 import React from "react";
 
-const AddTransformationTypePage = () => {
+import Header from "@/components/shared/header";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/transformation-form";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+
+  const transformation = transformationTypes[type];
+
+  if (!userId) return redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
   return (
-    <Header title="Transformation Title" subtitle="Transformation subtitle" />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
   );
 };
 
